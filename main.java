0a1683f3603f613be1e893a3d36e8cf2b1d3b764
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-05-27 15:54:56 +0300

    Update main.java

--- main.java
+++ main.java
@@ -11,22 +11,22 @@ public class main {
 		String MyName1 = myObj.nextLine();
 		
 		System.out.println("Great you're name is " + MyName1);
-		//name section is done 
+		//name section is done!
 		
 		System.out.println("What is you're username?");
 		String MyUsername = myObj.nextLine();
 		System.out.println("Nice, you're username is " + MyUsername);
-		//username section done
+		//username section done!
 		
 		System.out.println("What is you're age?");
 		String MyAge = myObj.nextLine();
 		System.out.println("Alright, you're age is " + MyAge);
-		//age section done
+		//age section done!
 		
 		System.out.println("What is you're sex?");
 		String MySex = myObj.nextLine();
 		System.out.println("Nice, you're username is " + MySex);
-		//sex section done 
+		//sex section done!
 		
 		System.out.println("You're id is ready");
 		
